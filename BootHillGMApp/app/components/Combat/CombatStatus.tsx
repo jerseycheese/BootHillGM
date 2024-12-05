42a16385c613f345eb31eb6fcbe7172dca0eb3de
Jack <jack.haas@civicactions.com>
2024-12-05 09:14:01 -0500

    feat: Add useState import and prepare for dynamic combat status rendering

--- BootHillGMApp/app/components/Combat/CombatStatus.tsx
+++ BootHillGMApp/app/components/Combat/CombatStatus.tsx
@@ -13,7 +13,7 @@
 
 'use client';
 
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import { Character } from '../../types/character';
 import { calculateCurrentStrength } from '../../utils/strengthSystem';
 import { cleanCharacterName } from '../../utils/combatUtils';
